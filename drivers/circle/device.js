0aadfbf6885177d990256082bbc57a9bfea16e51
Bas van den Bosch <bvdbos@gmail.com>
2026-04-26 10:02:16 +0200

    totaalverbruik opgenomen

--- drivers/circle/device.js
+++ drivers/circle/device.js
@@ -7,7 +7,11 @@ class CircleDevice extends Homey.Device {
 
   async onInit() {
     this.log(`Circle Device ${this.getName()} is geïnitialiseerd (v1.1)`);
-
+// Controleer of de capability 'meter_power' ontbreekt en voeg deze toe indien nodig
+  if (!this.hasCapability('meter_power')) {
+    this.log(`Adding missing capability: meter_power to ${this.getName()}`);
+    await this.addCapability('meter_power').catch(this.error);
+  }
     // Luister naar de aan/uit knop in de Homey App
     this.registerCapabilityListener('onoff', async (value) => {
       const ip = this.homey.settings.get('ip_address');
@@ -50,35 +54,51 @@ class CircleDevice extends Homey.Device {
 
   // Deze functie wordt aangeroepen vanuit de Driver
 onDataUpdate(myBlock) {
-  // 1. Status (on/off) uitlezen uit de XML van de Stretch
-  const stateMatch = myBlock.match(/<state>(on|off)<\/state>/);
-  const xmlIsOn = stateMatch ? (stateMatch[1] === 'on') : null;
+  this.log(`--- Update voor: ${this.getName()} ---`);
 
-  // 2. Wattage (W) uitlezen uit de XML
-  const measureMatch = myBlock.match(/<type>electricity_consumed<\/type>\s*<unit>W<\/unit>[\s\S]*?<measurement[^>]*?>([\d.]+)<\/measurement>/);
-
-  if (measureMatch) {
-    let power = parseFloat(measureMatch[1]);
-
-    // De Slimme Check:
-    // We dwingen 0W alleen af als:
-    // Homey zegt dat hij UIT staat EN de XML van de Stretch ook zegt dat hij UIT staat.
-    // Dit voorkomt dat 'ijling' (oude verbruikscijfers) de 0W overschrijft.
-    if (this.getCapabilityValue('onoff') === false && xmlIsOn === false) {
-      if (power > 0) {
-        // this.log('IJling gedetecteerd: Stretch zegt UIT maar stuurt nog Wattage. Correctie naar 0W.');
-        power = 0;
-      }
-    }
-
-    if (!isNaN(power)) {
-      this.setCapabilityValue('measure_power', power).catch(this.error);
-    }
+  // 1. Wattage (consumed W)
+  // We zoeken specifiek naar het blok waar 'electricity_consumed' boven de unit 'W' staat
+  const powerMatch = myBlock.match(/electricity_consumed<\/type>\s*<unit>W<\/unit>[\s\S]*?<measurement[^>]*?>([\d.]+)</);
+  if (powerMatch) {
+    const power = parseFloat(powerMatch[1]);
+    this.log(`[Watt] ${power} W`);
+    this.setCapabilityValue('measure_power', power).catch(this.error);
   }
 
-  // 3. Update de aan/uit status in Homey met de werkelijke status van de Stretch
-  if (xmlIsOn !== null) {
-    this.setCapabilityValue('onoff', xmlIsOn).catch(this.error);
+  // 2. Totaalverbruik (consumed Wh)
+  // We zoeken specifiek naar 'electricity_consumed' boven de unit 'Wh'
+  const intervalMatch = myBlock.match(/electricity_consumed<\/type>\s*<unit>Wh<\/unit>[\s\S]*?<measurement[^>]*?>([\d.]+)</);
+  if (intervalMatch) {
+    const currentHourWh = parseFloat(intervalMatch[1]);
+    let totalMeterKwh = this.getCapabilityValue('meter_power') || 0;
+    let lastKnownHourWh = this.getStoreValue('last_hour_wh');
+
+    if (lastKnownHourWh === null || lastKnownHourWh === undefined) {
+      this.log(`[kWh] Eerste meting: ${currentHourWh / 1000} kWh`);
+      this.setCapabilityValue('meter_power', currentHourWh / 1000).catch(this.error);
+    } else if (currentHourWh > lastKnownHourWh) {
+      const diffKwh = (currentHourWh - lastKnownHourWh) / 1000;
+      totalMeterKwh += diffKwh;
+      this.setCapabilityValue('meter_power', totalMeterKwh).catch(this.error);
+    }
+    
+    this.setStoreValue('last_hour_wh', currentHourWh).catch(this.error);
+    this.log(`[kWh] Stand: ${totalMeterKwh.toFixed(4)} kWh`);
+  }
+
+  // 3. Status (Aan/Uit)
+  // We kijken naar de actuator relay state
+  const relayMatch = myBlock.match(/<relay[\s\S]*?<state>(on|off)<\/state>/);
+  if (relayMatch) {
+    const isOn = relayMatch[1] === 'on';
+    this.setCapabilityValue('onoff', isOn).catch(this.error);
+  }
+
+  // 4. Datums
+  const stretchDateMatch = myBlock.match(/<created_date>([\d-T:.]+)/);
+  if (stretchDateMatch) {
+    const stretchDate = new Date(stretchDateMatch[1]).toLocaleDateString('nl-NL');
+    this.setSettings({ install_date_stretch: stretchDate }).catch(this.error);
   }
 }
 
