64e91309f71e96560d337d62eac461d9817b8084
Bas van den Bosch <bvdbos@gmail.com>
2026-04-26 08:54:02 +0200

    v1.1.1: Fix voor na-ijlen verbruik

--- drivers/circle/device.js
+++ drivers/circle/device.js
@@ -49,24 +49,38 @@ class CircleDevice extends Homey.Device {
   }
 
   // Deze functie wordt aangeroepen vanuit de Driver
-  onDataUpdate(myBlock) {
-    // 1. Wattage (W)
-    const regex = /<type>electricity_consumed<\/type>\s*<unit>W<\/unit>[\s\S]*?<measurement[^>]*?>([\d.]+)<\/measurement>/;
-    const match = myBlock.match(regex);
-    
-    if (match) {
-      const power = parseFloat(match[1]);
-      if (!isNaN(power)) {
-        this.setCapabilityValue('measure_power', power).catch(this.error);
+onDataUpdate(myBlock) {
+  // 1. Status (on/off) uitlezen uit de XML van de Stretch
+  const stateMatch = myBlock.match(/<state>(on|off)<\/state>/);
+  const xmlIsOn = stateMatch ? (stateMatch[1] === 'on') : null;
+
+  // 2. Wattage (W) uitlezen uit de XML
+  const measureMatch = myBlock.match(/<type>electricity_consumed<\/type>\s*<unit>W<\/unit>[\s\S]*?<measurement[^>]*?>([\d.]+)<\/measurement>/);
+
+  if (measureMatch) {
+    let power = parseFloat(measureMatch[1]);
+
+    // De Slimme Check:
+    // We dwingen 0W alleen af als:
+    // Homey zegt dat hij UIT staat EN de XML van de Stretch ook zegt dat hij UIT staat.
+    // Dit voorkomt dat 'ijling' (oude verbruikscijfers) de 0W overschrijft.
+    if (this.getCapabilityValue('onoff') === false && xmlIsOn === false) {
+      if (power > 0) {
+        // this.log('IJling gedetecteerd: Stretch zegt UIT maar stuurt nog Wattage. Correctie naar 0W.');
+        power = 0;
       }
     }
 
-    // 2. Status (Aan/Uit)
-    const stateMatch = myBlock.match(/<state>(on|off)<\/state>/);
-    if (stateMatch) {
-      this.setCapabilityValue('onoff', stateMatch[1] === 'on').catch(this.error);
+    if (!isNaN(power)) {
+      this.setCapabilityValue('measure_power', power).catch(this.error);
     }
   }
+
+  // 3. Update de aan/uit status in Homey met de werkelijke status van de Stretch
+  if (xmlIsOn !== null) {
+    this.setCapabilityValue('onoff', xmlIsOn).catch(this.error);
+  }
+}
 
 } // <--- Deze sloot waarschijnlijk niet goed af
 
